7ec9232c2fb1c55c585686e26fab43f06c220ce0
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-07-21 18:13:32 +0200

    Add buttonss and ul litst to var in js file

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
+/*
+    Wzór elementu listy:
+        <li>
+
+            <h1>Task 1</h1>
+            <button>Delete</button>
+            <button>Complete</button>
+        </li>
+*/
+
+
+// Pobieram ul
+
+var parentUl = document.getElementById('taskList');
+
+// Pobieram przycisk dodający element
+
+var addTask = document.getElementById('addTaskButton');
+
+// Pobieram input z którego będą pobierane informacje do listy
+
+var taskInput = document.getElementById('taskInput');
+
+
 
