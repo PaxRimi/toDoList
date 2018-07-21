d57d7bccab8bf638826a9d2df8aeb84baa2b774d
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-07-21 19:46:46 +0200

    Add condition to length off input text

--- js/app.js
+++ js/app.js
@@ -32,39 +32,45 @@ addTask.addEventListener('click', function () {
     // pobieram wartość inputa do li
     var text = taskInput.value;
 
-    // Tworzę nowy element li do listy
-    var li = document.createElement('li');
+    if (text.length > 5 && text.length < 100) {
+
+        // Tworzę nowy element li do listy
+        var li = document.createElement('li');
 
 
-    // Tworzę dwa przyciski do elementu li
-    // 1 Usuwający
-    var buttonDel = document.createElement('button');
-    buttonDel.innerText = "Delete";
-    // Dodaje funkcję do buttona usuwającego
-    buttonDel.addEventListener('click', function () {
-        parentUl.removeChild(this.parentElement);
-    });
-    // 2 Zatwierdzający zadanie
-    var buttonComp = document.createElement('button');
-    buttonComp.innerText = "Complete";
-    //Dodaje funkcję do buttona complete
-    buttonComp.addEventListener('click', function () {
-        var parentClass = Array.from(this.parentElement.classList);
+        // Tworzę dwa przyciski do elementu li
+        // 1 Usuwający
+        var buttonDel = document.createElement('button');
+        buttonDel.innerText = "Delete";
+        // Dodaje funkcję do buttona usuwającego
+        buttonDel.addEventListener('click', function () {
+            parentUl.removeChild(this.parentElement);
+        });
+        // 2 Zatwierdzający zadanie
+        var buttonComp = document.createElement('button');
+        buttonComp.innerText = "Complete";
+        //Dodaje funkcję do buttona complete
+        buttonComp.addEventListener('click', function () {
+            var parentClass = Array.from(this.parentElement.classList);
             console.log(parentClass);
-        if ( parentClass.indexOf('complete') === -1) {
-            this.parentElement.classList.add('complete')
-        } else {
-            this.parentElement.classList.remove('complete');
-        }
-    });
-    //Dodaję tekst i buttony do li
-    li.innerText = text;
-    li.appendChild(buttonComp);
-    li.appendChild(buttonDel);
+            if (parentClass.indexOf('complete') === -1) {
+                this.parentElement.classList.add('complete')
+            } else {
+                this.parentElement.classList.remove('complete');
+            }
+        });
+        //Dodaję tekst i buttony do li
+        li.innerText = text;
+        li.appendChild(buttonComp);
+        li.appendChild(buttonDel);
 
-    // Dodaje stworzony guzik do listy
-    parentUl.appendChild(li);
-    taskInput.value = "";
+        // Dodaje stworzony guzik do listy
+        parentUl.appendChild(li);
+        taskInput.value = "";
+    } else {
+        taskInput.value = "";
+        alert("Błąd");
+    }
 });
 
 // Dodaje funkcje usuwającą wykonane zadania
