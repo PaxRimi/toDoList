2d4c7d82658102963bb58dedf7fa0014b949c19d
Wojciech Lewandowski <lewandowski.wojciech93@gmail.com>
2018-07-21 18:40:47 +0200

    Add function that delete li with task

--- js/app.js
+++ js/app.js
@@ -22,4 +22,29 @@ var addTask = document.getElementById('addTaskButton');
 var taskInput = document.getElementById('taskInput');
 
 
+// Dodaje funkcję dodawania elementu do listy
+
+addTask.addEventListener('click', function () {
+    // pobieram wartość inputa do li
+    var text = taskInput.value;
+
+    // Tworzę nowy element li do listy
+    var li = document.createElement('li');
+
+
+    // Tworzę dwa przyciski do elementu li
+    // 1 Usuwający
+    var buttonDel = document.createElement('button');
+    buttonDel.innerText = "Delete";
+    // Dodaje funkcję do buttona usuwającego
+    buttonDel.addEventListener('click', function () {
+        parentUl.removeChild(this.parentElement;);
+    });
+    // 2 Zatwierdzający zadanie
+    var buttonComp = document.createElement('button');
+    buttonComp.innerText = "Complete";
+
+
+});
+
 
